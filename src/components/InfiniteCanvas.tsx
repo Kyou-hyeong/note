6d6f7c8d51ecec5191ec43e42253f56863875c2b
kkh0108.kwon <kkh0108.kwon@gmail.com>
2025-06-27 00:55:17 +0000

    Feat pointer dragging

--- src/components/InfiniteCanvas.tsx
+++ src/components/InfiniteCanvas.tsx
@@ -89,7 +89,7 @@ const InfiniteCanvas: React.FC = () => {
     if (!canvas) return;
     const ctx = canvas.getContext('2d');
     if (!ctx) return;
-  
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   
     ctx.save();
